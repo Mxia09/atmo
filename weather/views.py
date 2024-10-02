3e818d3437904d51b05310a452849543c40514f5
Marvin Xia <marvinxia09@gmail.com>
2024-10-02 15:56:11 -0700

    index displaying api data

--- weather/views.py
+++ weather/views.py
@@ -7,10 +7,25 @@ from decouple import config
 #     return render(requests, 'index.html')
 
 
-def index(request):
+# def index(request):
+#     api_key = config('OPENWEATHER_API_KEY')
+#     url = f'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={api_key}'
+#     city = 'San Francisco'
+#     city_weather = requests.get(url.format(city)).json()
+#     print(city_weather)
+#     return render(request, 'index.html')
+
+def index(request): 
     api_key = config('OPENWEATHER_API_KEY')
     url = f'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={api_key}'
     city = 'San Francisco'
     city_weather = requests.get(url.format(city)).json()
     print(city_weather)
-    return render(request, 'index.html')
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+    context = {'weather': weather}
+    return render(request, 'index.html', context)
