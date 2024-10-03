05532d24248fb5001e45d5243fb0ec0867c2c7ba
Marvin Xia <marvinxia09@gmail.com>
2024-10-03 15:55:35 -0700

    City model template

--- weather/views.py
+++ weather/views.py
@@ -3,18 +3,6 @@ import requests
 from decouple import config
 
 
-# def index(requests):
-#     return render(requests, 'index.html')
-
-
-# def index(request):
-#     api_key = config('OPENWEATHER_API_KEY')
-#     url = f'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={api_key}'
-#     city = 'San Francisco'
-#     city_weather = requests.get(url.format(city)).json()
-#     print(city_weather)
-#     return render(request, 'index.html')
-
 def index(request): 
     api_key = config('OPENWEATHER_API_KEY')
     url = f'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={api_key}'
