b0117da94c41b9a77531c20dab3e34e094f1b7cc
Marvin Xia <marvinxia09@gmail.com>
2024-10-30 15:52:57 -0700

    city template views

--- weather/views.py
+++ weather/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 import requests
 from decouple import config
 from weather.forms import CityForm
+from weather.models import City
 
 def index(request): 
     api_key = config('OPENWEATHER_API_KEY')
     url = f'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid={api_key}'
-    # city = 'San Francisco'
-    # city_weather = requests.get(url.format(city)).json()
-    # print(city_weather)
-    form = CityForm()
-    # weather = {
-    #     'city' : city,
-    #     'temperature' : city_weather['main']['temp'],
-    #     'description' : city_weather['weather'][0]['description'],
-    #     'icon' : city_weather['weather'][0]['icon']
-    # }
+    cities = City.objects.all()
+    # form = CityForm()
     weather_data = []
-    context = {'weather_data': weather_data, 'form': form}
+    for city in cities:
+        city_weather = requests.get(url.format(city)).json()
+        
+        weather = {
+            'city': city, 
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+        
+        weather_data.append(weather)
+        
+    context = {'weather_data': weather_data}
     return render(request, 'index.html', context)
