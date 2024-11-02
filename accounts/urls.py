feec0c7224e5eb5d5b21d432060ac1513e43d496
Marvin Xia <marvinxia09@gmail.com>
2024-11-01 17:51:57 -0700

    user login function and input form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from accounts.views import user_logout
+from accounts.views import user_logout, user_login
 
 urlpatterns = [
     path("logout/", user_logout, name="logout"),
+    path("login/", user_login, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.models import User
+from accounts.forms import LoginForm
+
 
 def user_logout(request):
     logout(request)
-    return redirect("")
+    return redirect("index.html")
+
+def user_login(request): 
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            
+            user = authenticate(request, username=username, password=password)
+            if user is not None: 
+                login(request, user)
+                return redirect("home")
+            else: 
+                form = LoginForm()
+            context = {"form": form}
+            return render(request, "accounts/login.html", context)
