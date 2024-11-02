512240eecc1c923a1f65a4fddfa235da4b1daf05
Marvin Xia <marvinxia09@gmail.com>
2024-11-01 20:47:11 -0700

    user signup function

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,7 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(max_length=150, widget=forms.PasswordInput)
     
+    
 class SignUpForm(forms.Form): 
     username = forms.CharField(max_length=150)
     email = forms.CharField(max_length=150)
